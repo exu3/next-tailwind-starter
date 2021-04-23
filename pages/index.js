703a491434fb7594babfc74019aa198f46b9678b
Ella <72365100+eilla1@users.noreply.github.com>
2021-04-22 17:04:35 -0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,26 +1,18 @@
-import { useEffect } from "react";
 import HeadObject from "../components/head";
 import Nav from "../components/nav";
 
 export default function Home() {
-  useEffect(() => {
-    console.log(
-      "Hello World"
-    );
-  });
 
   return (
     <div className="dark:text-white dark:bg-black">
-      <HeadObject>
-        {/* You can put extra tags in here, or leave it blank */}
-      </HeadObject>
+      <HeadObject />
       <Nav />
       <div className="text-xl text-center">
         <h1 className="leading-loose">
           You are viewing the Next-Tailwind theme starter.
         </h1>
         <button className="rounded-md bg-blue-300 dark:bg-blue-500 p-2 px-4 shadow-md mx-auto hover:shadow-lg">
-          <a href="https://github.com/eilla1/next-tailwind-starter">Github</a>
+          <a href="https://github.com/eilla1/next-starter">Github</a>
         </button>
       </div>
     </div>
