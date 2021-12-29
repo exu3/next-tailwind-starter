120b08de78ec9b29b18b071cb84999c1c92ea7c4
eilla1 <git@ella.cx>
2021-12-29 15:40:57 -0800

    Add custom fonts wip

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,18 @@
+/* eslint-disable no-undef */
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 module.exports = {
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
-  darkMode: 'class',
+  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  darkMode: "class",
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        mono: ["'custom mono font'", ...defaultTheme.fontFamily.mono],
+      },
+    },
   },
   variants: {
     extend: {},
   },
   plugins: [],
-}
+};
