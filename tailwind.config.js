74330d3e8a88f23d244c7fec23d74bc2ed034f03
Ella <72365100+eilla1@users.noreply.github.com>
2022-01-14 20:03:07 -0800

    purge -> content
    https://github.com/tailwindlabs/tailwindcss/discussions/6019

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@
 const defaultTheme = require("tailwindcss/defaultTheme");
 
 module.exports = {
-  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  content: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   darkMode: "class",
   theme: {
     extend: {
